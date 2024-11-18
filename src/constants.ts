c6edbd4ae923e5e2152a3f4886bdd40650598e40
Essential Randomness <essentialrandomn3ss@gmail.com>
2024-11-18 15:21:59 -0800

    fix rkeys and name of the label

--- src/constants.ts
+++ src/constants.ts
@@ -1,54 +1,54 @@
 import { Label } from './types.js';
 
-export const DELETE = 'insert-rkey-of-delete-post-here';
+export const DELETE = '3lax5qxhx2a2a';
 export const LABEL_LIMIT = 1;
 export const LABELS: Label[] = [
   {
-    rkey: 'insert-rkey-here',
+    rkey: '3lax5qwq2vv2q',
     identifier: 'slug',
     post: "🪱 Slug: A×B and only A×B (has a fixed OTP: a favorite ship, with no switching and no multishipping)",
     locales: [
-      { lang: 'en', name: '🪱 Slug', description: 'A×B and only A×B'},
+      { lang: 'en', name: '🪱 Slug 腐人', description: 'Ships A×B and only A×B'},
     ]
   },
   {
-    rkey: 'insert-rkey-here',
+    rkey: '3lax5qwt4ud2k',
     identifier: 'snake',
     post: "🐍 Snake: anyone × B (has a fixed uke: multiships, but their favorite can only bottom)",
     locales: [
-      { lang: 'en', name: '🐍 Snake', description: 'Anyone × B'},
+      { lang: 'en', name: '🐍 Snake 腐人', description: 'Ships Anyone × B'},
     ]
   },
   {
-    rkey: 'insert-rkey-here',
+    rkey: '3lax5qww6oq2a',
     identifier: 'frog',
     post: "🐸 Frog: A×B & B×A but no A×C (has a reversible OTP: no multishipping, but can accept switching in their favorite ship)",
     locales: [
-      { lang: 'en', name: '🐸 Frog', description: 'A×B & B×A but no A×C'},
+      { lang: 'en', name: '🐸 Frog 腐人', description: 'Ships A×B & B×A but no A×C'},
     ]
   },
   {
-    rkey: 'insert-rkey-here',
+    rkey: '3lax5qwzazx2p',
     identifier: 'gecko',
     post: "🦎 Gecko: A × anyone (\"seme freak\" who only wants their favorite to top)",
     locales: [
-      { lang: 'en', name: '🦎 Gecko', description: 'A × anyone'},
+      { lang: 'en', name: '🦎 Gecko 腐人', description: 'Ships A × anyone'},
     ]
   },
   {
-    rkey: 'insert-rkey-here',
+    rkey: '3lax5qxboce2h',
     identifier: 'snail',
     post: "🐌 Snail: anything × B (uses a mob character as seme for their favorite uke, either because there are no options in canon or because they only care about the one character; tentacles are also an option)",
     locales: [
-      { lang: 'en', name: '🐌 Snail', description: 'anything × B'}
+      { lang: 'en', name: '🐌 Snail 腐人', description: 'Ships anything × B'}
     ]
   },
   {
-    rkey: 'insert-rkey-here',
+    rkey: '3lax5qxeuq32h',
     identifier: 'turtle',
     post: "🐢 Turtle: omnivore, will try anything (can accept any ship in any order)",
     locales: [
-      { lang: 'en', name: '🐢 Turtle', description: 'anyone x anyone'}
+      { lang: 'en', name: '🐢 Turtle 腐人', description: 'Ships anyone x anyone'}
     ]
   },
 ];
