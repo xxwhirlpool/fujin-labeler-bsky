ce725d223bdd9ea3791c3cd8bef07c4ae5bc07b7
xxwhirlpool <witnesslachesis@disroot.org>
2025-08-06 21:49:56 +0000

    TYPO SABOTAGING MY BUILD SERVER

--- modules/nixos/fujin-labeler-bsky/default.nix
+++ modules/nixos/fujin-labeler-bsky/default.nix
@@ -136,7 +136,7 @@ in {
             forceSSL = true;
         
             locations."/" = {
-            	proxyWebsockets = true
+            	proxyWebsockets = true;
                 proxyPass = "http://127.0.0.1:${toString cfg.port}";
             };
         }; 
